738b922366284611432019766f675acb942ffc14
Alexey Golubev <alexey.sergeevich.golubev@gmail.com>
2021-08-01 21:10:26 +0300

    Перенесена инициализация sentry в settings/prod.py

--- images/admin-panel/src/config/settings/prod.py
+++ images/admin-panel/src/config/settings/prod.py
@@ -1,8 +1,14 @@
 # noinspection PyUnresolvedReferences
 import os
 
+import sentry_sdk
 from config.settings.base import *
 
 ALLOWED_HOSTS = ['*']
 
-SENTRY_URL = os.environ.get('SENTRY_URL', None)
+SENTRY_URL = os.environ.get('SENTRY_URL', "https://6f0e6c17ccec41d6a58229df1c34b807@o828822.ingest.sentry.io/5883947")
+sentry_sdk.init(
+    SENTRY_URL,
+    server_name="admin-panel",
+    traces_sample_rate=1.0
+)
